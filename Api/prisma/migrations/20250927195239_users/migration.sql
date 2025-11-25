80cb745efc77460c32f7d308ccb4d1d1cca8e169
IcaroAS-20 <icaroalvessilva84@gmail.com>
2025-11-24 21:58:30 -0300

    Update migration.sql

--- Api/prisma/migrations/20250927195239_users/migration.sql
+++ Api/prisma/migrations/20250927195239_users/migration.sql
@@ -5,6 +5,7 @@ CREATE TABLE "public"."Usuario" (
     "serie" TEXT NOT NULL,
     "ra" TEXT NOT NULL,
     "senha" TEXT NOT NULL,
+    "curso" TEXT NOT NULL,
     "criadoEm" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
@@ -12,3 +13,4 @@ CREATE TABLE "public"."Usuario" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Usuario_ra_key" ON "public"."Usuario"("ra");
+
